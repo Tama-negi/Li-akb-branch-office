2da0f47bf4f3bd9d914fc2a56f00229e9f8cb9b5
Li-akb-branch-office <44279548+Tama-negi@users.noreply.github.com>
2019-12-03 14:21:53 +0900

    Add files via upload

---
+++ Azure_PowerShell/VM_Start_SelectDiskType.ps1
@@ -0,0 +1,25 @@
+#VM Start Disk Type Select
+
+param (
+        [string] [Parameter(Mandatory=$true)]  $vmName
+      )
+
+$resourceGroupName ="RG Name"
+$storageType ="StandardSSD_LRS"
+#$vmName ="VM Name"
+
+
+$VM = Get-AzVM -Name $vmName -resourceGroupName $resourceGroupName
+$vmDisks = Get-AzDisk | Where { $_.ResourceGroupName -eq $resourceGroupName } | Where { $_.ManagedBy -eq $VM.id }
+
+foreach ($disk in $vmDisks)
+{
+    if ($disk.ManagedBy -eq $vm.Id)
+        {
+        $diskUpdateConfig = New-AzDiskUpdateConfig –AccountType $storageType
+        Update-AzDisk -DiskUpdate $diskUpdateConfig -ResourceGroupName $resourceGroupName `
+        -DiskName $disk.Name
+    }
+}
+
+Start-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
---
+++ Azure_PowerShell/Vm_One_Size_up_For_Linux.ps1
@@ -0,0 +1,33 @@
+#Vm one size Up Linux
+
+#RG Name
+$resourceGroupName = "RG Name"
+$VmName = "Vm Name"
+
+# VM Setting
+$VmCfg = get-azvm -ResourceGroupName $resourceGroupName -Name $VmName
+
+# Now VM Size
+$VmSize = ( $VmCfg.hardwareprofile | grep Standard |awk '{print $1}')
+
+Write-Host 'VmSize is '  $VmSize  ' '
+
+# New VM Size
+$New_VmSize  = ""
+$New_VmSize = ( `
+Get-AzVMSize -ResourceGroupName $resourceGroupName -VMName $VmName `
+| Sort-Object -property NumberOfCores,MemoryInMB,ResourceDiskSizeInMB `
+| grep $VmSize.Substring(0,10) `
+| grep -A 1 $VmSize.Substring(0,$VmSize.Length) `
+| awk 'NR== 2 {print $1}' )
+
+# New Vm Update Ok
+if($New_VmSize -eq $null) {
+    Write-Host 'No Vm Size'
+}else{
+    Write-Host 'New VM Size Is '  $New_VmSize  ' '
+
+$vmCfg.HardwareProfile.VmSize = $New_VmSize
+Update-AzVM -VM $vmCfg -ResourceGroupName $resourceGroupName
+
+}
