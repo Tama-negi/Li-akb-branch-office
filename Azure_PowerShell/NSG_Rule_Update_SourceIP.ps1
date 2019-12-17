a9735e8dbb525f30943fa2c5883ec391ec9ebe3f
Li-akb-branch-office <44279548+Tama-negi@users.noreply.github.com>
2019-12-17 14:15:18 +0900

    Add files via upload

---
+++ Azure_PowerShell/NSG_Rule_Update_SourceIP.ps1
@@ -0,0 +1,27 @@
+#NSG Rule Update(SourceAddressPrefix)
+
+param (
+    [string] [Parameter(Mandatory=$true)]  $NSGName,
+    [string] [Parameter(Mandatory=$true)]  $NSGRuleName,
+    [string] [Parameter(Mandatory=$true)]  $Address
+    )
+
+$resourceGroupName = "AKBWG-CSP-RG-NW"
+
+# Get the NSG resource
+$nsg = Get-AzNetworkSecurityGroup -Name $NSGName -ResourceGroupName $resourceGroupName
+
+# New Prefix List 
+$newPrefix = New-Object 'System.Collections.Generic.List[string]'
+
+# Existing Prefix Add
+($nsg.SecurityRules | where {$_.Name -eq $NSGRuleName}).SourceAddressPrefix | foreach { $newPrefix.Add($_) }
+
+# New Prefix Add
+$newPrefix.Add($Address)
+
+# New Prefix Rewrite
+($nsg.SecurityRules | where {$_.Name -eq $NSGRuleName}).SourceAddressPrefix = $newPrefix
+
+# Reflect changes
+$nsg | Set-AzNetworkSecurityGroup
